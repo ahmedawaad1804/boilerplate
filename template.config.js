f5dbcf7e66bcd4af6c12c1b9eacc2442fa3d2d8f
ahmedawaad1804 <ahmedawaad1804@gmail.com>
2021-06-05 15:50:31 +0200

    w

--- template.config.js
+++ template.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // Placeholder used to rename and replace in files
   // package.json, index.json, android/, ios/
-  placeholderName: "wadz",
+  placeholderName: "test",
 
   // Directory with template
   templateDir: "./test",
